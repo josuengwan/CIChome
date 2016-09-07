efa154da901c1b25f35bfd4c98081e6f6d56f0ea
HongJeSeong <howl78@naver.com>
2016-09-07 16:34:28 +0900

    re

--- templates/contact/contact/contact.php
+++ templates/contact/contact/contact.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" type="text/css" href="/home/assets/css/header.css">
     <link rel="stylesheet" type="text/css" href="/home/assets/css/footer.css">
     <link rel="stylesheet" type="text/css" href="/home/assets/css/contact.css">
- 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+ 	
 </head>
 <body>
 <!-- End Of Footer -->
@@ -23,23 +23,18 @@
 	        <tr style="height:50px;">
 	            <td>
 	                <div class="contact-item">
-	                    <div class="title">Contact</div>
-	              <div class="bar"></div>		
+	                    <div class="title">Contact</div>		
+	             <div class="bar"></div>		
 	                </div>
 	            </td>
 	        </tr>
-	       <tr style="height:350px;">
-	           <td>
-			<div id="map" >
-			<iframe src="https://www.google.com/maps/place/36%C2%B037'17.0%22N+127%C2%B017'10.8%22E/@36.6212322,127.2851293,17.25z/data=!4m5!3m4!1s0x0:0x0!8m2!3d36.621386!4d127.286347?hl=en-US"width="600"height="400"></iframe> <!-- googleMapAPI-->
-			</div>
-		  </td>
-	       </tr>
 	   </table>
+<div id="map"style="width:70%; height:400px;"></div>
     </div>
     <div class="contact-content">
 	   <table border="0" cellpadding="10" cellspacing="0" class="contact-table">
         <tr style="height:40px;">
+
             <td><span class="contact-title">Address</span><br><span>&nbsp;</span></td>
             <td><span class="kor-address">세종특별자치시 조치원읍 세종로 2639(신안리300) 홍익대학교 D407</span><br><span class="eng-address">D407, Hongik University, 2639 Sejong-ro(300 Sinan-ri), Jochiwon-eup, Sejong Special Self-Governing City, Republic of Korea</span></td>
         </tr>
@@ -98,6 +93,18 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-66909995-1', 'auto');
   ga('send', 'pageview');
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 36.621402, lng: 127.286354},
+    zoom: 17
+  });
+}
+
+   
 </script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZ5ks45D7YdnhGZQuJvc6IpQY8dsRW920&callback=initMap"
+        async defer></script>
 </body>
 </html>
